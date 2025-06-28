96e896bcfb331ea4f8f8c93b849ecaf012171a52
Urvansh-Frontend <urvanshgogna@gmail.com>
2025-06-28 19:43:33 +0000

    data types done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const name = "Urvansh"
+// name = "Kishna"  NOT WORKING DUE TO CONST CAN'T BE CaLLED AGAIN AND AGAIN
+
+let lastName = "Gogna"
+lastName = "Verma"
+
+var socialMedia = "Facebook"
+socialMedia = 'Snapchat'
+
+{
+  let cityName = "Chandigarh"
+  // console.log(cityName)
+}
+
+// cityName = "Delhi"
+
+
+
+console.table([name ,lastName , socialMedia , cityName])
+
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all code as New code now not required 
+
+// console.log(3 + 3) ;
+
+let name = "Urvansh" //String
+let age = 22 //Number
+let Married = false //Boolean
+
+
+console.log(typeof undefined)
+console.log(typeof null) // it  is object 
+
+//  THESE ARE PRIMITIVE 
+
+// NUMBER 
+// STRING
+// BIGINT (SOME TIME USED)
+// BOOLEAN
+// null (Something is their but empty now) Server Temperature 
+// undefined  (Not Definded now)
+// symbol (Mostly used in REACT)
+
+
+//  THESE ARE NON-PRIMITIVE 
+// ARRAY 
+// Object 

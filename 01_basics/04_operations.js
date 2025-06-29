05e6fc6305566a4bf47f5bf60964b6bc0243056f
Urvansh-Frontend <urvanshgogna@gmail.com>
2025-06-29 14:05:01 +0000

    prefix and postfix

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,19 @@
+let value =  3 ;
+let negvalue = -value;
+
+// console.log(negvalue);
+
+// console.log( '1' + 3);
+// console.log( '1' + 3 + 3);
+// console.log(  (3 + 3) + 1);
+// console.log( 3 + '1');
+
+// console.log(true);
+// console.log(+"");
+
+let gameCounter = 100;
+
+let x = 3;
+const y = x++;
+
+console.log(x);

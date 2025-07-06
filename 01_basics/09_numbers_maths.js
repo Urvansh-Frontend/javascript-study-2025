9d1f914221f1e5e6ac5cb0ffe93c4355d1ce6906
Urvansh-Frontend <urvanshgogna@gmail.com>
2025-07-06 18:24:57 +0000

    maths and numbers are done

---
+++ 01_basics/09_numbers_maths.js
@@ -0,0 +1,35 @@
+// const score = 400;
+// const balance = new Number(244);
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const profit = 250.5;
+// console.log(profit.toPrecision(3));
+
+// const hundreds = 10000;
+// console.log(hundreds.toLocaleString('en-in'))
+
+
+
+// MATHS STARTS FROM HERE 
+// console.log(Math);
+
+
+
+// console.log(Math.abs(-1291));   // Used for converting negative to positive value 
+// console.log(Math.round(6.4));   // Used to Round of the Value
+// console.log(Math.ceil(6.2));   // Round of value convert to next number
+// console.log(Math.floor(6.6));   // Round of value convert to same number
+
+// console.log(Math.ceil(Math.random()*10) + 1 );   
+
+const min = 10;
+const max = 20;
+
+const latest = (Math.random() * ((max - min) + 1)) + min;
+
+console.log(latest);

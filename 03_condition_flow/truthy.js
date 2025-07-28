661d8b69d2d6bec609fce490515160d29f794791
Urvansh-Frontend <urvanshgogna@gmail.com>
2025-07-28 18:08:23 +0000

    Conditonal flow is done

--- 03_condition_flow/truthy.js
+++ 03_condition_flow/truthy.js
@@ -1,9 +1,9 @@
-const username = ""
+const username = ''
 
 if (username) {
-    console.log("SUCCESS");
+    // console.log("SUCCESS");
 } else {
-    console.log("FAIL");
+    // console.log("FAIL");
 
 }
 
@@ -29,15 +29,31 @@ if (username) {
 
 
 
-if (username.length === 0) {
+if (username.length === 10) {
     console.log("ARRAY IS EMPTY")
 }
 
-const emptyobj = {}
+const emptyobj = { user: true, }
 
 if (Object.keys(emptyobj).length === 0) {
-    console.log("OBJECT IS EMPTY");
+    // console.log("OBJECT IS EMPTY");
 
 }
 
-// Nullish Coalescing Operator (??) : Null undefined
+// Nullish Coalescing Operator (??) : (Null) (undefined)
+
+let val1;
+// val1 = 11 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 188
+val1 = undefined ?? 900 ?? 155
+console.log(val1)
+
+//  Terniary operator - One Liner true and false     
+
+// condition ? true : false
+
+const goldprice = 500
+
+goldprice >= 200 ? console.log("YES IT IS ABOVE 200 ") : console.log('no it is less than 200')
+
